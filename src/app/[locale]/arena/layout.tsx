733ec5f6717952339e582303445ef262374c3720
tejeoner34 <atejezar@gmail.com>
2025-03-02 15:04:15 +0900

    fix: remove unnecessary margin

--- src/app/[locale]/arena/layout.tsx
+++ src/app/[locale]/arena/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ArenaProvider>
       <div className="w-full grid place-items-center">
-        <div className="w-full max-w-[1000px] m-4 flex flex-col justify-center gap-4 relative border-[10px] border-solid border-white">
+        <div className="w-full max-w-[1000px] flex flex-col justify-center gap-4 relative border-[10px] border-solid border-white">
           {children}
         </div>
       </div>
